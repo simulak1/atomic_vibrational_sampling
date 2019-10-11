1653799af066a9fe02015b075b6491518d21d5e2
Simula Kristoffer <ksimula@lx9-500-11306.acclab.helsinki.fi>
2019-10-11 13:05:04 +0300

    Added two new cmd line arguments to define te sampling statistics

--- main.py
+++ main.py
@@ -18,6 +18,26 @@ def get_args():
         type=float
     )
 
+    args_parser.add_argument(
+        '--sample-lim',
+        help="""
+        Spatial limits for the atomic position sampling. 
+        Sampling points between sample-lim*[-latvec,latvec].
+        """,
+        default=0.1,
+        type=float
+    )
+
+    args_parser.add_argument(
+        '--num-configs',
+        help=
+        """
+        The number of different occupational configs to sample.
+        """,
+        default=10,
+        type=int
+    )
+
     return args_parser.parse_args()
 
 def main():
